00a1a66f4e09d970d7f103d7fd2bae391f596b18
apodworny <alex.podworny@hotmail.com>
2025-08-20 02:33:07 +0200

    Added RemoveCrosshair mod

---
+++ RemoveCrosshair/PluginConsts.cs
@@ -0,0 +1,8 @@
+namespace RemoveCrosshair;
+
+public static class PluginConsts
+{
+    public const string PLUGIN_GUID = "apodworny.RemoveCrosshair";
+    public const string PLUGIN_NAME = "RemoveCrosshair";
+    public const string PLUGIN_VERSION = "1.0.0";
+}
