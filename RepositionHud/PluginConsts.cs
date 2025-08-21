5f314d87aeed10013ba542c97e7550ed1fba258f
apodworny <alex.podworny@hotmail.com>
2025-08-21 16:40:43 +0200

    Fixed typo in plugin guid

--- RepositionHud/PluginConsts.cs
+++ RepositionHud/PluginConsts.cs
@@ -2,7 +2,7 @@ namespace RepositionHud;
 
 public static class PluginConsts
 {
-    public const string PLUGIN_GUID = "--apodworny.RepositionHud";
+    public const string PLUGIN_GUID = "apodworny.RepositionHud";
     public const string PLUGIN_NAME = "RepositionHud";
     public const string PLUGIN_VERSION = "1.0.0";
 }
