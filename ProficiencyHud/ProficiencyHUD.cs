3b3390f0d7955504d30414b38cf95d722eab23a7
apodworny <alex.podworny@hotmail.com>
2025-08-24 00:10:21 +0200

    Added total proficiency level counter

--- ProficiencyHud/ProficiencyHUD.cs
+++ ProficiencyHud/ProficiencyHUD.cs
@@ -14,6 +14,7 @@ public class ProficiencyHUD : MonoBehaviour
     public static AssetBundle assets;
     private List<ProficiencyDisplay> proficiencyDisplays = new List<ProficiencyDisplay>();
 
+    private int _totalProficiencyLevel = 0;
     private const int ProficiencyHUDWidth = 156;
     private const int ProficiencyHUDHeight = 226;
 
@@ -27,6 +28,8 @@ public class ProficiencyHUD : MonoBehaviour
 
         ConfigureProficiencyHUDBase(canvas, proficiencyHUDBase);
         InitProficiencyDisplays(proficiencyHUDBase);
+
+        ConfigureTotalProficiencyLevel(proficiencyHUDBase);
 
         RepositionProficiencyDisplays();
     }
@@ -81,6 +84,9 @@ public class ProficiencyHUD : MonoBehaviour
             proficiencyDisplay.Initialize(proficiencyHUDBase, proficiency, progressToNextLevel, level);
 
             proficiencyDisplays.Add(proficiencyDisplay);
+
+            // Update the total proficiency level
+            _totalProficiencyLevel += int.Parse(proficiencyDisplay.levelLabel.text);
         }
     }
 
@@ -98,6 +104,36 @@ public class ProficiencyHUD : MonoBehaviour
 
             proficiencyDisplays[i].proficiencyDisplayTransform.anchoredPosition = new Vector2((ProficiencyDisplay.ProficiencyDisplayXSize + 19) / 2 + (col * horizontalSpacing), -(ProficiencyDisplay.ProficiencyDisplayYSize + 10) / 2 - (row * verticalSpacing));
         }
+    }
+
+    private void ConfigureTotalProficiencyLevel(GameObject proficiencyHUDBase)
+    {
+        GameObject totalProficiencyLevelGameObject = new GameObject("TotalProficiencyLevel", typeof(RectTransform));
+
+        // Attach the totalProficiencyLevelGameObject to the proficiencyHUDBase
+        totalProficiencyLevelGameObject.transform.SetParent(proficiencyHUDBase.transform, false);
+
+        Text levelLabel = totalProficiencyLevelGameObject.AddComponent<Text>();
+        levelLabel.fontSize = 16;
+        levelLabel.color = Color.white;
+        levelLabel.font = assets.LoadAsset<Font>("runescape_uf");
+        if (levelLabel.font == null)
+        {
+            Plugin.Log.LogInfo("Font failed to load from asset bundle!");
+        }
+        levelLabel.alignment = TextAnchor.MiddleCenter;
+
+        RectTransform totalLevelLabelTransform = levelLabel.GetComponent<RectTransform>();
+        totalLevelLabelTransform.anchorMin = new Vector2(1, 0);
+        totalLevelLabelTransform.anchorMax = new Vector2(1, 0);
+
+        // To set the label to be beside the icon,
+        // set the x of the anchoredPosition to the width of the icon plus half the width of the label
+        // and the y to minus half the height
+        totalLevelLabelTransform.sizeDelta = new Vector2(50, 50);
+        totalLevelLabelTransform.anchoredPosition = new Vector2(-30, 23);
+
+        levelLabel.text = $"Total:\n{_totalProficiencyLevel}";
     }
 
     private void LoadAssetBundle()
