94fc1fec3c23a363c3b006bda0ed2f7ec869fad3
apodworny <alex.podworny@hotmail.com>
2025-08-23 17:17:20 +0200

    Fully generalized proficiency displays, added images

--- ProficiencyHud/Components/ProficiencyDisplay.cs
+++ ProficiencyHud/Components/ProficiencyDisplay.cs
@@ -1,22 +1,143 @@
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using Awaken.TG.Main.General.StatTypes;
+
+namespace ProficiencyHUD;
 
 public class ProficiencyDisplay : MonoBehaviour
 {
     public Image proficiencyIcon;
     public TextMeshProUGUI levelLabel;
     public TextMeshProUGUI xpLabel;
+    private Sprite iconSprite;
+    public RectTransform proficiencyDisplayTransform;
+    private GameObject levelGameObject;
+    private GameObject iconGameObject;
+    private GameObject xpBarGameObject;
+    private GameObject xpBarProgressGameObject;
+    public ProfStatType profStatType;
 
-    public void SetProficiencyDisplay(Sprite icon, int level, float xpPercent)
+    private Sprite xpBarSprite = ProficiencyHUD.assets.LoadAsset<Sprite>("ProgressBar");
+
+    public RectTransform xpBarProgressTransform;
+    public static int ProficiencyDisplayXSize = 50;
+    public static int ProficiencyDisplayYSize = 30;
+
+    public void Initialize(GameObject proficiencyHUDBase, ProfStatType profStatType, float progressToNextLevel, string level)
     {
-        // if (proficiencyIcon != null)
-        //     proficiencyIcon.sprite = icon;
+        transform.SetParent(proficiencyHUDBase.transform, false);
 
-        // if (levelLabel != null)
-        //     levelLabel.text = $"Level: {level}";
+        this.profStatType = profStatType;
 
-        // if (xpLabel != null)
-        //     xpLabel.text = $"XP: {Mathf.RoundToInt(xpPercent * 100)}%";
+        ConfigureProficiencyDisplay();
+        ConfigureIcon();
+        ConfigureLevel(level);
+        ConfigureXPBar();
+        ConfigureXPBarProgress(progressToNextLevel);
+
+        xpBarProgressTransform = xpBarProgressGameObject.GetComponent<RectTransform>();
+    }
+
+    private void ConfigureProficiencyDisplay()
+    {
+        proficiencyDisplayTransform = GetComponent<RectTransform>();
+        proficiencyDisplayTransform.sizeDelta = new Vector2(ProficiencyDisplayXSize, ProficiencyDisplayYSize);
+
+        Image proficiencyBaseImage = gameObject.AddComponent<Image>();
+        proficiencyBaseImage.color = new Color(6f/255f, 6f/255f, 6f/255f, 1f);
+        proficiencyDisplayTransform.anchorMin = new Vector2(0, 1);
+        proficiencyDisplayTransform.anchorMax = new Vector2(0, 1);
+    }
+
+    private void ConfigureIcon()
+    {
+        iconGameObject = new GameObject($"{profStatType.EnumName}Icon", typeof(RectTransform));
+
+        // Attach to the parent
+        iconGameObject.transform.SetParent(gameObject.transform, false);
+
+        iconSprite = ProficiencyHUD.assets.LoadAsset<Sprite>(profStatType.EnumName);
+
+        Image iconImage = iconGameObject.AddComponent<Image>();
+        iconImage.sprite = iconSprite;
+
+        RectTransform iconRect = iconGameObject.GetComponent<RectTransform>();
+
+        // Set the anchor to the top left corner of its parent
+        iconRect.anchorMin = new Vector2(0, 1);
+        iconRect.anchorMax = new Vector2(0, 1);
+
+        // Need to set the image size depending on the image
+        iconRect.sizeDelta = new Vector2(24, 24);
+
+        // To set the icon to be at the top left corner,
+        // set the x of the anchoredPosition to half the width of the icon and the y to -half the inferred height
+        iconRect.anchoredPosition = new Vector2(12, -12);
+    }
+
+    private void ConfigureLevel(string level)
+    {
+        levelGameObject = new GameObject($"{profStatType.EnumName}LevelLabel", typeof(RectTransform));
+
+        // Attach to the parent
+        levelGameObject.transform.SetParent(gameObject.transform, false);
+
+        levelLabel = levelGameObject.AddComponent<TextMeshProUGUI>();
+        levelLabel.fontSize = 14;
+        levelLabel.color = Color.white;
+
+        RectTransform levelLabelTransform = levelLabel.GetComponent<RectTransform>();
+        levelLabelTransform.anchorMin = new Vector2(0, 1);
+        levelLabelTransform.anchorMax = new Vector2(0, 1);
+
+        // To set the label to be beside the icon,
+        // set the x of the anchoredPosition to the width of the icon plus half the width of the label
+        // and the y to minus half the height
+        levelLabelTransform.sizeDelta = new Vector2(20, 20);
+        levelLabelTransform.anchoredPosition = new Vector2(24 + 10, -10);
+
+        levelLabel.text = level;
+    }
+
+    private void ConfigureXPBar()
+    {
+        xpBarGameObject = new GameObject($"{profStatType.EnumName}XPBar", typeof(RectTransform));
+
+        // Attach to the parent
+        xpBarGameObject.transform.SetParent(gameObject.transform, false);
+
+        Image xpBarImage = xpBarGameObject.AddComponent<Image>();
+        xpBarImage.sprite = xpBarSprite;
+
+        RectTransform agilityXPBackgroundRect = xpBarGameObject.GetComponent<RectTransform>();
+
+        // Set the anchor to the top left corner of its parent
+        agilityXPBackgroundRect.anchorMin = new Vector2(0, 1);
+        agilityXPBackgroundRect.anchorMax = new Vector2(0, 1);
+
+        agilityXPBackgroundRect.sizeDelta = new Vector2(42, 5);
+
+        // To set the icon to be at the top left corner,
+        // Set the x of the anchoredPosition to half the width of the icon
+        // and the y to minus the inferred height of the icon minus half the height
+        agilityXPBackgroundRect.anchoredPosition = new Vector2(21f, -24f - 2.5f);
+    }
+
+    private void ConfigureXPBarProgress(float progressToNextLevel)
+    {
+        xpBarProgressGameObject = new GameObject($"{profStatType.EnumName}XPBarProgress", typeof(RectTransform));
+
+        // Attach to the parent
+        xpBarProgressGameObject.transform.SetParent(xpBarGameObject.transform, false);
+
+        Image proficiencyBaseImage = xpBarProgressGameObject.AddComponent<Image>();
+        proficiencyBaseImage.color = Color.white;
+
+        xpBarProgressTransform = xpBarProgressGameObject.GetComponent<RectTransform>();
+        xpBarProgressTransform.anchorMin = new Vector2(0, 1);
+        xpBarProgressTransform.anchorMax = new Vector2(0, 1);
+        xpBarProgressTransform.sizeDelta = new Vector2(progressToNextLevel * 42f, 5);
+        xpBarProgressTransform.anchoredPosition = new Vector2(progressToNextLevel * 42f / 2, -2.5f);
     }
 }
--- ProficiencyHud/ProficiencyHUD.cs
+++ ProficiencyHud/ProficiencyHUD.cs
@@ -1,46 +1,30 @@
 using Awaken.TG.Main.Heroes;
-using Awaken.TG.MVC.Elements;
 using UnityEngine;
-using Awaken.TG.Assets;
-using TMPro;
-using Awaken.TG.MVC;
 using Awaken.TG.Main.General.StatTypes;
 using UnityEngine.UI;
+using System.Collections.Generic;
 
 namespace ProficiencyHUD;
 
 public class ProficiencyHUD : MonoBehaviour
 {
-    private TextMeshProUGUI agilityLabel;
     private Hero hero;
 
-    private int sneakPercentage;
-    private int agilityPercentage;
-
-    private Sprite sneakSprite;
-    private Sprite agilityIconSprite;
-    private Sprite agilityXPBarSprite;
-
-    RectTransform agilityXPBarTransform;
+    public static AssetBundle assets;
+    private List<ProficiencyDisplay> proficiencyDisplays = new List<ProficiencyDisplay>();
 
     void Start()
     {
+        LoadAssetBundle();
         GameObject proficiencyHUDBase = new GameObject("ProficiencyHUDBase", typeof(RectTransform));
-        GameObject agilityLabelGameObject = new GameObject("AgilityLabel", typeof(RectTransform));
-        GameObject agilityIconGameObject = new GameObject("AgilityIcon", typeof(RectTransform));
-        GameObject agilityXPBackgroundGameObject = new GameObject("AgilityXPBackground", typeof(RectTransform));
-        GameObject agilityXPBarGameObject = new GameObject("AgilityXPBar", typeof(RectTransform));
-
-        loadAssets();
 
         // Get the canvas from the hero
         Canvas canvas = GetComponentInParent<Canvas>();
 
-        configureProficiencyHUDBase(canvas, proficiencyHUDBase);
-        configureAgilityIcon(proficiencyHUDBase, agilityIconGameObject);
-        configureAgilityLabel(proficiencyHUDBase, agilityLabelGameObject);
-        configureAgilityXP(proficiencyHUDBase, agilityXPBackgroundGameObject);
-        configureAgilityXPBar(agilityXPBackgroundGameObject, agilityXPBarGameObject);
+        ConfigureProficiencyHUDBase(canvas, proficiencyHUDBase);
+        InitProficiencyDisplays(proficiencyHUDBase);
+
+        RepositionProficiencyDisplays();
     }
 
     public void SetHero(Hero hero)
@@ -51,130 +35,71 @@ public class ProficiencyHUD : MonoBehaviour
     public void UpdateProgress(ProfStatType typeofProfession, float progress)
     {
         int percentage = Mathf.FloorToInt(progress * 100f);
-        if (typeofProfession == ProfStatType.Acrobatics)
-        {
-            agilityPercentage = percentage;
-        }
-        else if (typeofProfession == ProfStatType.Sneak)
-        {
-            sneakPercentage = percentage;
-        }
 
-        // if (agilityLabel != null)
-        // {
-        //     agilityLabel.text = $"{hero.ProficiencyStats.Acrobatics.BaseValue}, {agilityPercentage}%\n\n{hero.ProficiencyStats.Sneak.BaseValue}, {sneakPercentage}%";
-        // }
+        ProficiencyDisplay proficiencyDisplay = proficiencyDisplays.Find(display => display.profStatType == typeofProfession);
 
-        if (agilityLabel != null)
-        {
-            agilityLabel.text = hero.ProficiencyStats.Acrobatics.BaseValue.ToString();
+        proficiencyDisplay.levelLabel.text = typeofProfession._getter(hero).BaseValue.ToString();
 
-            // I have the percentage in an integer, but width of the background is 35, so it should be percentage of 35
-            agilityXPBarTransform.sizeDelta = new Vector2(agilityPercentage * 0.35f, 5);
-            agilityXPBarTransform.anchoredPosition = new Vector2(agilityPercentage * 0.35f / 2, -2.5f);
-        }
+        // I have the percentage in an integer, but width of the background is 42, so it should be percentage of 42
+        proficiencyDisplay.xpBarProgressTransform.sizeDelta = new Vector2(percentage * 0.42f, 5);
+        proficiencyDisplay.xpBarProgressTransform.anchoredPosition = new Vector2(percentage * 0.42f / 2, -2.5f);
     }
 
-    private void loadAssets()
-    {
-        string bundlePath = System.IO.Path.Combine(System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location), "proficiencyhud");
-        AssetBundle bundle = AssetBundle.LoadFromFile(bundlePath);
-
-        sneakSprite = bundle.LoadAsset<Sprite>("sneak");
-        agilityIconSprite = bundle.LoadAsset<Sprite>("agility");
-        agilityXPBarSprite = bundle.LoadAsset<Sprite>("progressbar");
-    }
-
-    private void configureProficiencyHUDBase(Canvas canvas, GameObject proficiencyHUDBase)
+    private void ConfigureProficiencyHUDBase(Canvas canvas, GameObject proficiencyHUDBase)
     {
         // Attach the proficiencyHUDBase to the hero's canvas
         proficiencyHUDBase.transform.SetParent(canvas.transform, false);
 
         RectTransform proficiencyHUDTransform = proficiencyHUDBase.GetComponent<RectTransform>();
-        proficiencyHUDTransform.sizeDelta = new Vector2(200, 200);
+        proficiencyHUDTransform.sizeDelta = new Vector2(300, 400);
 
         Image proficiencyBaseImage = proficiencyHUDBase.AddComponent<Image>();
-        proficiencyBaseImage.color = Color.black;
+        proficiencyBaseImage.color = new Color(0.1f, 0.1f, 0.1f, 0.8f);
         proficiencyHUDTransform.anchorMin = new Vector2(1, 0.5f);
         proficiencyHUDTransform.anchorMax = new Vector2(1, 0.5f);
-        proficiencyHUDTransform.anchoredPosition = new Vector2(-20, 0);
+        proficiencyHUDTransform.anchoredPosition = new Vector2(-150, 0);
     }
 
-    private void configureAgilityIcon(GameObject proficiencyHUDBase, GameObject agilityIconGameObject)
+    private void InitProficiencyDisplays(GameObject proficiencyHUDBase)
     {
-        agilityIconGameObject.transform.SetParent(proficiencyHUDBase.transform, false);
+        proficiencyDisplays.Clear();
 
-        Image agilityIconImage = agilityIconGameObject.AddComponent<Image>();
-        agilityIconImage.sprite = agilityIconSprite;
-        agilityIconImage.preserveAspect = true;
+        foreach (ProfStatType proficiency in ProfStatType.HeroProficiencies)
+        {
+            string level = proficiency._getter(hero).BaseValue.ToString();
+            float progressToNextLevel = hero.ProficiencyStats.GetProgressToNextLevel(proficiency);
 
-        RectTransform agilityIconRect = agilityIconGameObject.GetComponent<RectTransform>();
+            GameObject proficiencyDisplayGameObject = new GameObject($"{proficiency.EnumName}Display", typeof(RectTransform));
+            proficiencyDisplayGameObject.transform.SetParent(proficiencyHUDBase.transform, false);
 
-        // Set the anchor to the top left corner of its parent
-        agilityIconRect.anchorMin = new Vector2(0, 1);
-        agilityIconRect.anchorMax = new Vector2(0, 1);
+            ProficiencyDisplay proficiencyDisplay = proficiencyDisplayGameObject.AddComponent<ProficiencyDisplay>();
+            proficiencyDisplay.Initialize(proficiencyHUDBase, proficiency, progressToNextLevel, level);
 
-        agilityIconRect.sizeDelta = new Vector2(24, 24);
-
-        // To set the icon to be at the top left corner,
-        // set the x of the anchoredPosition to half the width of the icon and the y to -half the height
-        agilityIconRect.anchoredPosition = new Vector2(12, -12);
+            proficiencyDisplays.Add(proficiencyDisplay);
+        }
     }
 
-    private void configureAgilityLabel(GameObject proficiencyHUDBase, GameObject agilityLabelGameObject)
+    private void RepositionProficiencyDisplays()
     {
-        // Attach the agilityLabelGameObject to the proficiencyHUDBase
-        agilityLabelGameObject.transform.SetParent(proficiencyHUDBase.transform, false);
+        float verticalSpacing = 44f;
+        float horizontalSpacing = 44f;
+        int columns = 3;
 
-        agilityLabel = agilityLabelGameObject.AddComponent<TextMeshProUGUI>();
-        agilityLabel.fontSize = 12;
-        agilityLabel.color = Color.white;
+        for (int i = 0; i < proficiencyDisplays.Count; i++)
+        {
+            int row = i / columns;
+            int col = i % columns;
 
-        RectTransform agilityLabelTransform = agilityLabel.GetComponent<RectTransform>();
-        agilityLabelTransform.anchorMin = new Vector2(0, 1);
-        agilityLabelTransform.anchorMax = new Vector2(0, 1);
-
-        // To set the label to be beside the icon,
-        // set the x of the anchoredPosition to the width of the icon plus half the width of the label
-        // and the y to minus half the height
-        agilityLabelTransform.sizeDelta = new Vector2(20, 20);
-        agilityLabelTransform.anchoredPosition = new Vector2(24 + 10, -10);
+            proficiencyDisplays[i].proficiencyDisplayTransform.anchoredPosition = new Vector2((ProficiencyDisplay.ProficiencyDisplayXSize / 2) + (col * horizontalSpacing), -(ProficiencyDisplay.ProficiencyDisplayYSize / 2) - (row * verticalSpacing));
+        }
     }
 
-    private void configureAgilityXP(GameObject proficiencyHUDBase, GameObject agilityXPBackgroundGameObject)
+    private void LoadAssetBundle()
     {
-        // Attach the agilityXPBackgroundGameObject to the proficiencyHUDBase
-        agilityXPBackgroundGameObject.transform.SetParent(proficiencyHUDBase.transform, false);
-
-        Image agilityXPBackgroundImage = agilityXPBackgroundGameObject.AddComponent<Image>();
-        agilityXPBackgroundImage.sprite = agilityXPBarSprite;
-
-        RectTransform agilityXPBackgroundRect = agilityXPBackgroundGameObject.GetComponent<RectTransform>();
-
-        // Set the anchor to the top left corner of its parent
-        agilityXPBackgroundRect.anchorMin = new Vector2(0, 1);
-        agilityXPBackgroundRect.anchorMax = new Vector2(0, 1);
-
-        agilityXPBackgroundRect.sizeDelta = new Vector2(35, 5);
-
-        // To set the icon to be at the top left corner,
-        // Set the x of the anchoredPosition to half the width of the icon
-        // and the y to minus the height of the icon minus half the height
-        agilityXPBackgroundRect.anchoredPosition = new Vector2(17.5f, -24 - 2.5f);
-    }
-
-    private void configureAgilityXPBar(GameObject agilityXPBackgroundGameObject, GameObject agilityXPBarGameObject)
-    {
-        // Attach the agilityXPBackgroundGameObject to the agilityXPBackgroundGameObject
-        agilityXPBarGameObject.transform.SetParent(agilityXPBackgroundGameObject.transform, false);
-
-        agilityXPBarTransform = agilityXPBarGameObject.GetComponent<RectTransform>();
-        agilityXPBarTransform.sizeDelta = new Vector2(0, 5);
-
-        Image proficiencyBaseImage = agilityXPBarGameObject.AddComponent<Image>();
-        proficiencyBaseImage.color = Color.white;
-        agilityXPBarTransform.anchorMin = new Vector2(0, 1);
-        agilityXPBarTransform.anchorMax = new Vector2(0, 1);
-        agilityXPBarTransform.anchoredPosition = new Vector2(0, -2.5f);
+        if (assets == null)
+        {
+            string bundlePath = System.IO.Path.Combine(System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location), "proficiencyhud");
+            assets = AssetBundle.LoadFromFile(bundlePath);
+        }
     }
 }
