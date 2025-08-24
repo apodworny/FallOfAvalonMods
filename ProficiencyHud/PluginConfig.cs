17de5355bceb8ccd0dd3edac4a803a79d14f89d1
apodworny <alex.podworny@hotmail.com>
2025-08-24 13:05:33 +0200

    Added a config for changing where the proficiency hud is located vertically

--- ProficiencyHud/PluginConfig.cs
+++ ProficiencyHud/PluginConfig.cs
@@ -1,19 +1,21 @@
 using BepInEx.Configuration;
 
-namespace ProficiencyHUD;
-
 public class PluginConfig
 {
-    // TODO: add config if needed
-    // public ConfigEntry<bool> MyValue { get; private set; }
+    public ConfigEntry<float> ProficiencyHUDVerticalPosition { get; private set; }
 
     public PluginConfig(ConfigFile config)
     {
         config.SaveOnConfigSet = false;
         try
         {
-            // TODO: add config bindings as needed
-            // MyValue = config.Bind("Category", "ValueName", DefaultValue, "Description");
+            const string BarsCategory = "ProficiencyHUDPosition";
+            const string proficiencyHUDVerticalPositionDescription =
+                """
+                The vertical alignment of the proficiency HUD.
+                Value must be within 0 and 1, where 0 is the bottom and 1 is the top of the screen.
+                """;
+            ProficiencyHUDVerticalPosition = config.Bind(BarsCategory, nameof(ProficiencyHUDVerticalPosition), 0.4f, proficiencyHUDVerticalPositionDescription);
         }
         finally
         {
