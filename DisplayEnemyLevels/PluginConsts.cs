9dec47c23837e35415dba977507d43c736bc97c2
apodworny <alex.podworny@hotmail.com>
2025-08-24 15:46:36 +0200

    Init DisplayEnemyLevels mod

---
+++ DisplayEnemyLevels/PluginConsts.cs
@@ -0,0 +1,8 @@
+namespace DisplayEnemyLevels;
+
+public static class PluginConsts
+{
+    public const string PLUGIN_GUID = "apodworny.DisplayEnemyLevels";
+    public const string PLUGIN_NAME = "DisplayEnemyLevels";
+    public const string PLUGIN_VERSION = "1.0.0";
+}
