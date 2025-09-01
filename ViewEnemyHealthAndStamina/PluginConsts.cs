be9d147c1607cd196c860cf4578601bb59623119
apodworny <alex.podworny@hotmail.com>
2025-09-01 23:39:54 +0200

    Started new mod, ViewEnemyHealthAndStamina

---
+++ ViewEnemyHealthAndStamina/PluginConsts.cs
@@ -0,0 +1,8 @@
+namespace ViewEnemyHealthAndStamina;
+
+public static class PluginConsts
+{
+    public const string PLUGIN_GUID = "apodworny.ViewEnemyHealthAndStamina";
+    public const string PLUGIN_NAME = "ViewEnemyHealthAndStamina";
+    public const string PLUGIN_VERSION = "1.0.0";
+}
